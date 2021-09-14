2876d59ecc68b28364b7c2b950c453f1e6fbc0cc
zyh1996saa <86151357+zyh1996saa@users.noreply.github.com>
2021-09-14 22:17:19 +0800

    Add files via upload

--- pf_python/cascading.py
+++ pf_python/cascading.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append(r'C:\Users\zyh\Desktop\PYPOWER-master\pypower')
+sys.path.append(r'.\PYPOWER-master\pypower')
 import case39
 from runpf import runpf
 import numpy as np 
@@ -34,7 +34,39 @@ def init_case(case39):
     
     return copied_case39
     
+class Cascasding_39model():
+    def __init__(self):
+        self.init_case = runpf(init_case(case39.case39()))[0]
+        self.set_mapping()
+        self.K = 0.7
     
-a=case39.case39()
-inited_a = init_case(a)
-x=runpf(inited_a)[0]
+    def set_mapping(self):
+        self.mapping = {}
+        for i in range(self.init_case['bus'].shape[0]):
+            for j in range(self.init_case['gen'].shape[0]):
+                if int(self.init_case['gen'][j,0]) == int(self.init_case['bus'][i,0]) and j>9:
+                    self.mapping[str(i)] = j
+        
+    def set_short_fault(self,case,buses):
+        cp_case = case.copy()
+        for i in buses:
+            cp_case['gen'][int(self.mapping[str(i)]),2] = self.K*cp_case['gen'][int(self.mapping[str(i)]),1]
+            cp_case['gen'][int(self.mapping[str(i)]),1] = 0
+        return cp_case
+            
+            
+            
+    def run(self):
+        short_fault_set = [[15,16]]
+        for short_fault in short_fault_set:
+            begin_case = self.set_short_fault(self.init_case,short_fault)
+            begin_case = runpf(begin_case)
+        
+        return begin_case
+
+
+
+model_39 = Cascasding_39model()
+b = model_39.init_case
+a = runpf(model_39.init_case)[0]
+xx = model_39.run()[0]
